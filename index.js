100846116777656ca55b02b6a456a3864ee03f02
balka-n <harun.dzambo@gmail.com>
2025-09-30 20:35:08 +0200

    implemented radio buttons for acts
    each radio button will now either reveal/hide certain items, as to avoid spoiling the user.
    
    on first visit, every item will be hidden, with a footer additionally added, notifying the user about local storage usage.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let checkboxes = document.querySelectorAll('.check')
 let state = JSON.parse(localStorage.getItem('checklistState') || '{}');
 const percentText = document.getElementById('percent');
+let radioState = localStorage.getItem('actChoice');
 
 checkboxes.forEach(checkbox => {
     if (state[checkbox.id]) {
@@ -29,11 +30,178 @@ checkboxes.forEach(checkbox => {
 })
 
 function percentCalculate() {
-    const checkedCount = Object.values(state).filter(Boolean).length;
-    let percent = ((checkedCount / checkboxes.length) * 100).toFixed(0);
+    let totalWeight = 0;
+    let current = 0;
+
+    checkboxes.forEach(cb => {
+        const w = parseFloat(cb.dataset.weight || 1);
+        totalWeight += w;
+        if (state[cb.id])
+            current += w;
+    });
+    
+    let percent = ((current / totalWeight) * 100).toFixed(2);
     percentText.innerHTML = percent + '%'
     localStorage.setItem('checklistState', JSON.stringify(state));
 }
+
+let originals = {};
+
+for (let i = 1; i <= 20; i++){
+    const el = document.getElementById('boxmask' + i)
+    if (el) originals['boxmask' + i] = el.innerHTML
+}
+
+function actOne() {
+    document.getElementById('boxmask1').innerHTML = 'Bought from Pebb [ 300 Rosaries ]'
+    document.getElementById('boxtp1').innerHTML = 'Bought from Mort [ 220 Rosaries ]'
+    document.getElementById('boxtp2').innerHTML = '1st reward from Loddie'
+    document.querySelector('.flowerBlock').style.display = 'none'
+
+    for (let i = 7; i <= 20; i++){
+        id = 'boxmask' + i
+        el = document.getElementById(id);
+        cb = document.getElementById('mask' + i)
+
+        if (!originals[id]) {
+            originals[id] = el.innerHTML
+        }
+        el.innerHTML = '---'
+        cb.disabled = true;
+    }
+
+    for (let i = 2; i <= 3; i++){
+        id = 'boxheart' + i
+        el = document.getElementById(id);
+        cb = document.getElementById('heart' + i)
+
+        if (!originals[id]) {
+            originals[id] = el.innerHTML
+        }
+        el.innerHTML = '---'
+        cb.disabled = true;
+    }
+    
+    for (let i = 7; i <= 18; i++){
+        id = 'boxspool' + i
+        el = document.getElementById(id)
+        cb = document.getElementById('spool' + i)
+
+        if (originals[id]) {
+            originals[id] = el.innerHTML
+        }
+        el.innerHTML = '---'
+        cb.disabled = true;
+    }
+    
+    for (let i = 3; i <= 4; i++){
+        id = 'boxck' + i
+        el = document.getElementById(id);
+        cb = document.getElementById('ck' + i)
+
+        if (!originals[id]) {
+            originals[id] = el.innerHTML
+        }
+        el.innerHTML = '---'
+        cb.disabled = true;
+    }
+
+    for (let i = 4; i <= 4; i++){
+        id = 'boxtp' + i
+        el = document.getElementById(id);
+        cb = document.getElementById('tp' + i)
+
+        if (!originals[id]) {
+            originals[id] = el.innerHTML
+        }
+        el.innerHTML = '---'
+        cb.disabled = true;
+    }
+}
+
+function actTwo() {
+    document.querySelector('.flowerBlock').style.display = 'none'
+    document.getElementById('boxmask1').innerHTML = 'Bought from Pebb [ 300 Rosaries ]'
+    for (let i = 7; i <= 16; i++){
+        id = 'boxmask' + i
+        el = document.getElementById(id);
+        el.innerHTML = originals[id]
+    }
+
+    for (let i = 4; i <= 4; i++){
+        id = 'boxtp' + i
+        el = document.getElementById(id);
+        cb = document.getElementById('tp' + i)
+
+        if (!originals[id]) {
+            originals[id] = el.innerHTML
+        }
+        el.innerHTML = '---'
+        cb.disabled = true;
+    }
+
+    for (let i = 17; i <= 20; i++){
+        id = 'boxmask' + i
+        el = document.getElementById(id);
+        cb = document.getElementById('mask' + i)
+
+        if (!originals[id]) {
+            originals[id] = el.innerHTML
+        }
+        el.innerHTML = '---'
+        cb.disabled = true;
+    }
+}
+
+function actThree() {
+    document.querySelector('.flowerBlock').style.display = 'flex    '
+    document.getElementById('boxmask1').innerHTML = 'Bought from Grindle [ 320 Rosaries ]'
+    document.getElementById('boxtp1').innerHTML = 'Bought from Grindle [ 220 Rosaries ]'
+    document.getElementById('boxtp2').innerHTML = 'At Loddie\'s former location'
+}
+
+const actRadio = document.querySelectorAll('input[name="act"]');
+
+const saved = localStorage.getItem('actChoice');
+if (saved) {
+  const r = document.querySelector(`input[name="act"][value="${saved}"]`);
+  if (r) r.checked = true;
+}
+
+function updateActState() {
+  const chosen = document.querySelector('input[name="act"]:checked');
+  const enabled = !!chosen;
+  checkboxes.forEach(cb => cb.disabled = !enabled);
+    if (chosen) {
+        anchors.forEach(a => {
+            a.innerHTML = originals[a.id];
+        })
+        document.querySelector('.notif').style.display = 'none';
+      if (chosen.value === 'a1') actOne(); else if (chosen.value === 'a2') actTwo(); else actThree();
+    localStorage.setItem('actChoice', chosen.value);
+    }
+  else {
+      firstVisit();
+    }
+  
+}
+
+// first load
+let anchors = document.querySelectorAll('a:not(.link)');
+anchors.forEach(a => {
+    originals[a.id] = a.innerHTML;
+})
+
+function firstVisit() {
+    anchors.forEach(a => {
+        a.innerHTML = '---' 
+    })
+    document.querySelector('.flowerBlock').style.display = 'none'
+}
+
+
+updateActState();
+actRadio.forEach(r => r.addEventListener('change', updateActState));
 
 function loadStyles() {
     checkboxes.forEach(checkbox => {
